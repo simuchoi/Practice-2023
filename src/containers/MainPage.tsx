c81449d1abf25f57a8ef0feee1c84f509a15d181
Oleg Solovyov <simuchoi@mail.ru>
2023-06-19 22:39:25 +0300

    html + css v.1.0

---
+++ src/containers/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ExampleComponent } from '../components';
+
+export const MainPage = () => {
+  return (
+    <>
+      <ExampleComponent example="exampleText" />
+      или даже просто текст
+    </>
+  );
+};
